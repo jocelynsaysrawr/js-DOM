33f55473275b4f534c0965c5f1df81911d1ffa5f
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2017-07-19 21:13:12 -1000

    completed exercise 7

--- js/app.js
+++ js/app.js
@@ -61,7 +61,11 @@ Create a div element and give it an id of "name7". Inside this div element, give
 
 Append this div element to the nameParent div*/
 
+var newDiv = document.createElement("div");
+newDiv.id = "name7";
+newDiv.innerHTML = "Peter Griffin";
 
+nameParent.appendChild(newDiv);
  
 /*8. Tim Duncan
 
